ab9fc4ab1c8d25a53ee9ab8370a02dfc25876c4d
killer <michael.molchanov.2004@gmail.com>
2023-03-15 22:52:15 +0100

    finished titanic stuff

--- machine-l/lab/lab-1-14.03.2023/matstat/titanic.py
+++ machine-l/lab/lab-1-14.03.2023/matstat/titanic.py
@@ -18,5 +18,49 @@ print(df.columns)
 
 # Simple data analytics
 
+# Min
 min_row = df.loc[df['age'].idxmin()]
+print("-----Youngest person-----")
 print(min_row)
+
+# Max
+max_row = df.loc[df['age'].idxmax()]
+print("-----Oldest person-----")
+print(max_row)
+
+# Average
+average_age = df.T.loc['age'].mean()
+print("-----Average Age-----")
+print(average_age)
+
+# Stuff with women
+
+# first class women
+women_in_first_class = df[(df['sex'] == 'female') & (df['class'] == '1st')]
+print("-----First class girls-----")
+print(women_in_first_class)
+
+# Youngest girl
+youngest_woman = women_in_first_class['age'].min()
+print("-----Fbi plz don't open up i didn't know her age-----")
+print(youngest_woman)
+
+# More mature girl
+oldest_woman = women_in_first_class['age'].max()
+print("-----Mature woman-----")
+print(oldest_woman)
+
+# How many are still alive
+survived_count = women_in_first_class['survived'].replace({'yes': 1, 'no': 0}).sum()
+print("-----How many survived-----")
+print(survived_count)
+
+# drawing histogram of age
+plt.hist(df['age'], bins=50)
+
+# Let's add some labels, so it'll be a bit easier to read
+plt.title('Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Count')
+
+plt.savefig('age.png')
