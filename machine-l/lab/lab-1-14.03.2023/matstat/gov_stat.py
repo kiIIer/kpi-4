b6029fca42844a87664a25a9f42127d12241c00f
kiIIer <39669501+kiIIer@users.noreply.github.com>
2023-03-15 23:28:40 +0100

    Machine l/lab/1 (#40)
    * protocol setup
    
    * started gov stats
    
    * moved gov stat code
    
    * move data
    
    * rename data
    
    * gitignore for data folder
    
    * first try of stats
    
    * removed nan
    
    * added a little coment
    
    * good looking gov data
    
    * gov stats
    
    * i forgot median
    
    * preview titanic
    
    * rename column
    
    * min
    
    * finished titanic stuff
    
    * all in all
    
    * run func for titanic
    
    * final touches
    
    * lets pretend i did not forget about this

---
+++ machine-l/lab/lab-1-14.03.2023/matstat/gov_stat.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def run():
+    # Read Excel file
+    df = pd.read_excel('./data/gov_data.xlsx')
+
+    # Remove possible corrupted data
+    df.dropna(inplace=True)
+
+    # Drop old indexing
+    df.drop(columns=df.columns[0], inplace=True)
+    # Use year indexes
+    df.set_index('Year', inplace=True)
+
+    # Calculate and show statistics
+
+    print("-----MEAN-----")
+    print(df.mean())
+
+    print("-----MODE-----")
+    print(df.mode())
+
+    print("-----MEDIAN-----")
+    print(df.median())
+
+    print("-----VARIANCES-----")
+    print(df.var())
+
+    print("-----STD_DEVS-----")
+    print(df.std())
+
+    # Show histogram
+    df.hist(figsize=(20, 10))
+    plt.savefig('gov_hist.png')
+
+    # Get row wit loc
+    test_row = df.loc['2018']
+
+    # Get row with iloc
+    test_row = df.iloc[0]
+
+    # Get cell with names
+    cell = df.at['2018', 'total']
+
+    # cell but with iat
+    cell = df.at[0, 0]
