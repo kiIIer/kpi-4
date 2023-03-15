d16f7f64c930bc522ff3a5ebf4dd3645d1e8a03a
killer <michael.molchanov.2004@gmail.com>
2023-03-15 14:45:09 +0100

    added a little coment

--- machine-l/lab/lab-1-14.03.2023/matstat/gov_stat.py
+++ machine-l/lab/lab-1-14.03.2023/matstat/gov_stat.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 def run():
     # Read Excel file
     df = pd.read_excel('./data/gov_data.xlsx')
+
+    # Remove possible corrupted data
     df.dropna(inplace=True)
 
     # Calculate and show statistics
