c5e1033c3da670c03125c373e59f75e9bcd39109
kiIIer <39669501+kiIIer@users.noreply.github.com>
2023-04-16 18:28:54 +0200

    Machine l/lab/3 (#59)
    * predicting
    
    * part 1
    
    * rename main
    
    * one classifier
    
    * changed print
    
    * now changed it for real
    
    * part one
    
    * different classifiers
    
    * senond part done

---
+++ machine-l/lab/lab-3-13.04.2023/linreg2/lecex.py
@@ -0,0 +1,75 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(1)
+X_xor = np.random.randn(200, 2)
+Y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+Y_xor = np.where(Y_xor, 1, -1)
+plt.scatter(X_xor[Y_xor == 1, 0], X_xor[Y_xor == 1, 1], c='b', marker='x', label='1')
+plt.scatter(X_xor[Y_xor == -1, 0], X_xor[Y_xor == -1, 1], c='r', marker='s', label='-1')
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+plt.show()
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    markers = ('s', 'x', '0', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=colors[idx], marker=markers[idx], label=cl,
+                    )
+
+        if test_idx:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+            plt.scatter(X_test[:, 0], X_test[:, 1], c='', edgecolor='black', linewidth=1, marker='0',
+                        s=11)
+
+
+svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
+svm.fit(X_xor, Y_xor)
+plot_decision_regions(X_xor, Y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
+
+lin = SVC(kernel='linear', random_state=1, gamma=0.10, C=10.0)
+lin.fit(X_xor, Y_xor)
+plot_decision_regions(X_xor, Y_xor, classifier=lin)
+plt.legend(loc='upper left')
+plt.show()
+
+poly = SVC(kernel='poly', random_state=1)
+poly.fit(X_xor, Y_xor)
+plot_decision_regions(X_xor, Y_xor, classifier=poly)
+plt.legend(loc='upper left')
+plt.show()
+
+knei = KNeighborsClassifier()
+knei.fit(X_xor, Y_xor)
+plot_decision_regions(X_xor, Y_xor, classifier=knei)
+plt.legend(loc='upper left')
+plt.show()
+
+gauss = GaussianNB()
+gauss.fit(X_xor, Y_xor)
+plot_decision_regions(X_xor, Y_xor, classifier=gauss)
+plt.legend(loc='upper left')
+plt.show()
---
+++ machine-l/lab/lab-3-13.04.2023/linreg2/linr2.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+
+df = pd.read_csv(
+    'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/national/time-series/110/tavg/1/1/1895-2018.csv')
+
+df.drop(df.columns[2], axis=1, inplace=True)
+df.drop([0, 1, 2], inplace=True)
+df = df.reset_index(drop=True)
+df.columns = ['Date', 'Temperature']
+
+df[df.columns[0]] = df[df.columns[0]].astype(int)
+df[df.columns[1]] = df[df.columns[1]].astype(float)
+
+df.Date = df.Date.floordiv(100)
+
+print(df)
+
+X_train, X_test, y_train, y_test = train_test_split(df.Date.values.reshape(-1, 1), df.Temperature.values, test_size=0.2,
+                                                    random_state=42)
+
+print(X_train.shape)
+print(X_test.shape)
+
+linear_regression = LinearRegression()
+
+linear_regression.fit(X_train, y_train)
+
+print(linear_regression.coef_)
+print(linear_regression.intercept_)
+
+predicted = linear_regression.predict(X_test)
+
+expected = y_test
+
+for p, e in zip(predicted[::5], expected[::5]):
+    print(f'predicted: {p:.2f}, expected: {e:.2f}')
+
+predict = (lambda x: linear_regression.coef_ * x + linear_regression.intercept_)
+
+print(predict(2019))
+print(predict(1890))
+
+axes = sns.scatterplot(data=df, x='Date', y='Temperature', hue='Temperature', palette='winter', legend=False)
+axes.set_ylim(10, 70)
+
+x = np.array([min(df.Date.values), max(df.Date.values)])
+y = predict(x)
+
+line = plt.plot(x, y)
+
+plt.show()
