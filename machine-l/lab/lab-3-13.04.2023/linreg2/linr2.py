b2faff5b7ce9c7eff87585881ec272b02bd66c07
killer <michael.molchanov.2004@gmail.com>
2023-04-16 17:14:27 +0200

    now changed it for real

--- machine-l/lab/lab-3-13.04.2023/linreg2/linr2.py
+++ machine-l/lab/lab-3-13.04.2023/linreg2/linr2.py
@@ -24,11 +24,11 @@ X_train, X_test, y_train, y_test = train_test_split(df.Date.values.reshape(-1, 1
                                                     random_state=42)
 
 print(X_train.shape)
-print(y_train.shape)
+print(X_test.shape)
 
 linear_regression = LinearRegression()
 
-linear_regression.fit(X_train, X_test)
+linear_regression.fit(X_train, y_train)
 
 print(linear_regression.coef_)
 print(linear_regression.intercept_)
